7339e3b022660869f6b5234cf666d6f25b6116ed
amzxir <amirreza79ahmadi@gmail.com>
2024-01-26 16:16:51 +0330

    start data

--- src/assets/svg/chevronLeft.jsx
+++ src/assets/svg/chevronLeft.jsx
@@ -2,7 +2,7 @@ const ChevronLeft = () => {
     return (
         <svg width="20" height="20" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g filter="url(#filter0_d_1_331)">
-                <path d="M15 18L9 12L15 6" stroke="#434343" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M15 18L9 12L15 6" stroke="#434343" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </g>
         </svg>
 
